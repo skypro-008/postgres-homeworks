7f95c0cdd6cc367589f2450e922d44ffb47a7512
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-04-03 20:27:30 +0300

    add get_data_from_csv()

---
+++ homework-1/utils.py
@@ -0,0 +1,33 @@
+import csv
+
+
+def get_data_from_csv(key: str, path: str) -> list:
+    """Получает нужные данные из файла"""
+    with open(path, encoding='cp1251') as file:
+        reader = csv.DictReader(file)
+        data = []
+        if key == 'employees':
+            emp_id = 0
+            for row in reader:
+                emp_id += 1
+                name = (row['first_name'] + ' ' + row['last_name'])
+                profession = row['title']
+                notes = row['notes']
+                data.append((emp_id, name, profession, notes))
+
+        elif key == 'customers':
+            for row in reader:
+                customer_id = row['customer_id']
+                company_name = row['company_name']
+                data.append((customer_id, company_name))
+
+        elif key == 'orders':
+            for row in reader:
+                order_id = row['order_id']
+                customer_id = row['customer_id']
+                emp_id = row['employee_id']
+                date = row['order_date']
+                city = row['ship_city']
+                data.append((order_id, customer_id, emp_id, date, city))
+
+        return data
