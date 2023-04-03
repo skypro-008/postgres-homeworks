0ef24af291c58cca35825aef8274b56fab11cade
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-04-03 20:27:14 +0300

    add code for creating tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	employee_name varchar(100) NOT NULL,
+	profession varchar(100) NOT NULL,
+    notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY NOT NULL,
+	company_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id varchar(5) PRIMARY KEY NOT NULL,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employe_id),
+	order_date date NOT NULL,
+	city varchar(50) NOT NULL
+);
