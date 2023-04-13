39c577d82154c02a74c3f2bb196ba9bdc4477c34
Kn1ght16 <Danchenko0@mail.ru>
2023-04-13 14:54:45 +0300

    homework_2 fix 2

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -10,20 +10,20 @@ ORDER BY freight DESC
 LIMIT 10
 
 -- 3. фамилию и телефон сотрудников, у которых в данных отсутствует регион (см таблицу employees)
-SELECT last_name, home_phon FROM employees
+SELECT last_name, home_phone FROM employees
 WHERE region IS NULL
 
 -- 4. количество поставщиков (suppliers) в каждой из стран. Результат отсортировать по убыванию количества поставщиков в стране
 SELECT country, COUNT(*) as count_ FROM suppliers
 GROUP BY country
-ORDER BY count DESC
+ORDER BY count_ DESC
 
 -- 5. суммарный вес заказов (в которых известен регион) по странам, но вывести только те результаты, где суммарный вес на страну больше 2750. Отсортировать по убыванию суммарного веса (см таблицу orders, колонки ship_region, ship_country, freight)
 SELECT ship_country, SUM(freight) as sum_freight FROM orders
 WHERE ship_region IS NOT NULL
 GROUP BY ship_country
 HAVING SUM(freight) > 2750
-ORDER BY sum
+ORDER BY sum_freight
 
 -- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
 SELECT country FROM customers
