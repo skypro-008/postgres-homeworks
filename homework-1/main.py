88b7ba535779b117e83d6782db5a70731b76154f
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-04-19 13:29:11 +0300

    Дописал скрипт для заполнения остальных БД

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,8 +4,8 @@ import psycopg2
 
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 customers_data = './north_data/customers_data.csv'
-employees_data = './north_data/employees_data.'
-orders_data = './north_data/orders_data'
+employees_data = './north_data/employees_data.csv'
+orders_data = './north_data/orders_data.csv'
 
 connection = psycopg2.connect(
     host='localhost',
@@ -17,8 +17,10 @@ connection = psycopg2.connect(
 try:
     with connection:
         with connection.cursor() as cursor:
+
             with open(customers_data) as file:
                 file_reader = csv.DictReader(file, delimiter=',')
+
                 for i in file_reader:
                     customer_id = i.get('customer_id')
                     company_name = i.get('company_name')
@@ -26,8 +28,35 @@ try:
 
                     cursor.executemany(
                         'INSERT INTO customers VALUES (%s, %s, %s)',
-                        [(customer_id, company_name, contact_name)]
-                    )
+                        [(customer_id, company_name, contact_name)])
+
+            with open(employees_data) as file:
+                file_reader = csv.DictReader(file, delimiter=',')
+
+                for i in file_reader:
+                    first_name = i.get('first_name')
+                    last_name = i.get('last_name')
+                    title = i.get('title')
+                    birth_date = i.get('birth_date')
+                    notes = i.get('notes')
+
+                    cursor.executemany(
+                        'INSERT INTO employees VALUES (default, %s, %s, %s, %s, %s)',
+                        [(first_name, last_name, title, birth_date, notes)])
+
+            with open(orders_data) as file:
+                file_reader = csv.DictReader(file, delimiter=',')
+
+                for i in file_reader:
+                    order_id = i.get('order_id')
+                    customer_id = i.get('customer_id')
+                    employee_id = i.get('employee_id')
+                    order_date = i.get('order_date')
+                    ship_city = i.get('ship_city')
+
+                    cursor.executemany(
+                        'INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+                        [(order_id, customer_id, employee_id, order_date, ship_city)])
 
 except Exception as e:
     print(e)
@@ -35,4 +64,3 @@ except Exception as e:
 finally:
     if connection:
         connection.close()
-
