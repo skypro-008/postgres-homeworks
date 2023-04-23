78c4ad9939cc10f27464c5a5da2d773a5d2fb777
natalie <natalie@example.com>
2023-04-23 20:54:30 +0300

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY NOT NULL,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+CREATE TABLE orders
+(
+	order_id varchar(5) PRIMARY KEY NOT NULL,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
