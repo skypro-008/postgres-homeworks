ccc3eb67e84e274d165b4bfa802616ce7427727a
admi <nariman.ramazaov@yandex.ru>
2023-06-29 09:20:17 +0300

    done 'create_tables.sql'

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name CHARACTER VARYING(30),
+	last_name CHARACTER VARYING(30),
+	title CHARACTER VARYING(30),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id CHARACTER VARYING(30) PRIMARY KEY,
+	company_name text,
+	contact_name CHARACTER VARYING(30)
+);
+
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id CHARACTER VARYING(30) UNIQUE REFERENCES customers(customer_id),
+	employee_id int UNIQUE REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city CHARACTER VARYING(30)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+import pandas as pd
+
+def customers_d():
+    """Функция открывает customers_data.csv и для записи данных в базу данных postgresql
+    в таблицу customers"""
+
+    with open('north_data/customers_data.csv', encoding='utf-8') as file:
+        customers = csv.reader(file, delimiter=",")
+        for i in customers:
+            customer_id = i[0]
+            company_name = i[1]
+            contact_name = i[2]
+
+            with psycopg2.connect(host="localhost", database="north", user="admi", password="1234") as conn:
+                with conn.cursor() as cur:
+                    cur.execute('INSERT INTO customers VALUES(%s, %s, %s)', (customer_id, company_name, contact_name))
+                    cur.execute('SELECT * FROM customers')
+            conn.close()
+
+
+
+
+
+
+
+
