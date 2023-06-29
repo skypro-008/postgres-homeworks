3eae8fc1ee03f95a6c432ec8e1119cbba098f00e
admi <nariman.ramazaov@yandex.ru>
2023-06-29 09:33:26 +0300

    add func emloyees_d and customers_d

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,11 +8,11 @@ def customers_d():
     в таблицу customers"""
 
     with open('north_data/customers_data.csv', encoding='utf-8') as file:
-        customers = csv.reader(file, delimiter=",")
+        customers = csv.DictReader(file, delimiter=",")
         for i in customers:
-            customer_id = i[0]
-            company_name = i[1]
-            contact_name = i[2]
+            customer_id = i['customer_id']
+            company_name = i['company_name']
+            contact_name = i['contact_name']
 
             with psycopg2.connect(host="localhost", database="north", user="admi", password="1234") as conn:
                 with conn.cursor() as cur:
@@ -20,10 +20,22 @@ def customers_d():
                     cur.execute('SELECT * FROM customers')
             conn.close()
 
+def employees_d():
+    with open('north_data/employees_data.csv', encoding='utf-8') as file:
+        employees = csv.DictReader(file, delimiter=",")
+        for i in employees:
+            employee_id = i['employee_id']
+            first_name = i['first_name']
+            last_name = i['last_name']
+            title = i['title']
+            birth_date = i["birth_date"]
+            notes = i['notes']
 
-
-
-
-
+            with psycopg2.connect(host="localhost", database="north", user="admi", password="1234") as conn:
+                with conn.cursor() as cur:
+                    cur.execute('INSERT INTO employees VALUES(%s, %s, %s, %s, %s, %s)',
+                                (employee_id, first_name, last_name, title, birth_date, notes))
+                    cur.execute('SELECT * FROM employees')
+            conn.close()
 
 
