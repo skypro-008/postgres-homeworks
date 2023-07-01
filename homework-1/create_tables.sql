3e8a4006f68bfeca53f017f9e9415e61ca3856b6
Margarita Rastriga <rasm@1Ci.com>
2023-07-02 02:35:45 +0300

    Сделана загрузка из файлов

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id char(7) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title text,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(7) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(50) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,38 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+FILE_NAME1 = "./north_data/customers_data.csv"
+FILE_NAME2 = "./north_data/employees_data.csv"
+FILE_NAME3 = "./north_data/orders_data.csv"
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="123"
+)
+
+
+def read_file(file_name, table_name):
+
+    """Загружает данные из файла в таблицу"""
+
+    with open(file_name, mode="r", encoding='utf-8') as file_customers:
+        file_customers.readline()
+
+        for file_lines in file_customers.readlines():
+            try:
+                cleared_line = file_lines.replace("\\\n", "").replace("\'", "\'\'").replace("\"", "\'")
+                cur.execute(f"INSERT INTO {table_name} VALUES ({cleared_line})")
+            except Exception as e:
+                print(f"Пропускаю строку {file_lines}, ошибка {e}")
+
+if __name__ == '__main__':
+    try:
+        with conn.cursor() as cur:
+            read_file(FILE_NAME1, "customers")
+            read_file(FILE_NAME2, "employees")
+            read_file(FILE_NAME3, "orders")
+            conn.commit()
+    finally:
+        conn.close()
