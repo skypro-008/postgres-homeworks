dcecf6821e31b2c787b7fefe7c70618d24999073
denanmax <deshis93@gmail.com>
2023-07-04 22:41:23 +0200

    scripts for queries.sql added

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,35 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name, employees.first_name, employees.last_name
+FROM orders
+JOIN customers ON orders.customer_id = customers.customer_id
+JOIN employees ON orders.employee_id = employees.employee_id
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city='London' AND employees.city='London' AND shippers.company_name='United Package'
+
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products p
+JOIN suppliers s ON p.supplier_id = s.supplier_id
+JOIN categories c ON p.category_id = c.category_id
+WHERE p.discontinued = 0 AND p.units_in_stock < 25 AND (c.category_id = 2 OR c.category_id = 4)
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name
+FROM customers
+WHERE customer_id NOT IN (SELECT customer_id FROM orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT product_name
+FROM products
+WHERE product_id IN (SELECT * FROM order_details WHERE quantity = 10)
