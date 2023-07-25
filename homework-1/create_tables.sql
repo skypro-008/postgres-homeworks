9251a8edb7a545c1dbcec04db88961bd4fa15a09
Aleksandr Charushin <charushinas@gmail.com>
2023-07-25 15:21:00 +0300

    Created postgres commands to create tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id smallint PRIMARY KEY,
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	title varchar(255) NOT NULL,
+	birt_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(255) NOT NULL,
+	contract_name varchar(255) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id smallint REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(255) NOT NULL
+)
