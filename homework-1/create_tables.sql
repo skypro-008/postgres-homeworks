b370153f6c98093e418e5edceb357f3097b39e83
Grandic <d.bitkov@gmail.com>
2023-08-02 20:48:24 +0300

    Hometask1 was over

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,22 +1,35 @@
 -- SQL-команды для создания таблиц
 
+"""Создание cemployees"""
+
 CREATE TABLE employees
+"""Это правильное и логичное создание таблицы"
 (
 	employee_id int PRIMARY KEY,
-	first_name varchar(20) NOT NULL,
-	last_name varchar(20) NOT NULL,
-	title varchar(30) NOT NULL,
-	birth_date date NOT NULL,
-	notes text
+	first_name varchar(20),
+	last_name varchar(20),
+	title varchar(30),
+	birth_date date,
+	notes varchar(500)
+)
+"""Так я создал чтобы дланные могли передаватся в виде строк"""
+"""Это не совсем правильно, но copy не берет такое большое кол-во символов в столбце notes""""
+(
+	employee_id varchar(20),
+	first_name varchar(20),
+	last_name varchar(20),
+	title varchar(30),
+	birth_date varchar(20),
+	notes varchar(500)
 )
 
 """Создание customers_data"
 
 CREATE TABLE customers_data
 (
-	customer_id varchar(5) NOT NULL,
-	company_name varchar(40) NOT NULL,
-	contact_name varchar(30) NOT NULL
+	customer_id varchar(10),
+	company_name varchar(40),
+	contact_name varchar(30)
 )
 
 """Создание orders_data"
@@ -24,8 +37,8 @@ CREATE TABLE customers_data
 CREATE TABLE orders_data
 (
 	order_id int PRIMARY KEY,
-	customer_id varchar(5) NOT NULL,
-	employee_id int NOT NULL,
-	order_date date NOT NULL,
-	ship_city varchar(20) NOT NULL
+	customer_id varchar(10),
+	employee_id int,
+	order_date date,
+	ship_city varchar(20)
 )
--- homework-1/main.py
+++ homework-1/main.py
@@ -2,4 +2,26 @@
 import psycopg2
 import csv
 
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='qm4p00ed'
+)
+cursor = conn.cursor()
 
+with open('north_data/employees_data.csv', 'r', encoding="utf-8") as f:
+    next(f)
+    cursor.copy_from(f, 'employees', sep=',')
+
+with open('north_data/employees_data.csv', mode='r') as f:
+    count = 0
+    csvFile = csv.reader(f)
+    for row in csvFile:
+        cursor.execute("INSERT INTO employees VALUES(%s, %s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+with open('north_data/orders_data.csv', 'r', encoding="utf-8") as f:
+    next(f)
+    cursor.copy_from(f, 'orders_data', sep=',')
+
+conn.commit()
