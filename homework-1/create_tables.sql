1781e86aa1e20c56767f5a48029e497c6d0b6872
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2023-08-06 21:39:14 +0300

    first commit

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar(100) primary key,
+	company_name varchar(100) not null,
+	contact_name varchar(100) not null
+);
+
+CREATE TABLE employees
+(
+	employee_id int primary key,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	title varchar(100) not null,
+	birth_date varchar(100) not null,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int primary key,
+	customer_id varchar(100) references customers(customer_id) not null,
+	employee_id int references employees(employee_id) not null,
+	order_date varchar(100) not null,
+	ship_city varchar(100) not null
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,48 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# имена файлов
+customers_data = "customers_data.csv"
+employees_data = "employees_data.csv"
+orders_data = "orders_data.csv"
+
+
+def get_list_from_file(path):
+    with open(f"north_data/{path}", "r", encoding="utf-8") as f:
+        file_list = csv.reader(f)
+        total = []
+        for el in file_list:
+            total.append(el)
+
+    return total
+
+
+customers_data_list = get_list_from_file(customers_data)
+employees_data_list = get_list_from_file(employees_data)
+orders_data_list = get_list_from_file(orders_data)
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="12345"
+)
+cur = conn.cursor()
+
+
+def fill_table(data_list, name_table):
+    data_list = data_list[1:]
+
+    for el in data_list:
+        string_config = "%s, " * len(data_list[0])
+        cur.execute(f"INSERT INTO {name_table} VALUES ({string_config[:-2]})", tuple(el))
+
+
+fill_table(customers_data_list, "customers")
+fill_table(employees_data_list, "employees")
+fill_table(orders_data_list, "orders")
+
+conn.commit()
+
+cur.close()
+conn.close()
