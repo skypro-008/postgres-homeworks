7543a95cf7cc90f0a2614b57056920b71264b228
Vyacheslav Tr9khimyuk <tva2023github@gmail.com>
2023-08-06 23:40:42 +0300

    Add main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,63 +3,65 @@
 import psycopg2
 
 
-employees_csv = "../homework-1/north_data/employees_data.csv"
-customers_csv = "../homework-1/north_data/customers_data.csv"
-orders_csv = "../homework-1/north_data/orders_data.csv"
+if __name__ == '__main__':
 
-# Подкючение к базе данных north
-conn = psycopg2.connect(host='localhost',
+    employees_csv = "../homework-1/north_data/employees_data.csv"
+    customers_csv = "../homework-1/north_data/customers_data.csv"
+    orders_csv = "../homework-1/north_data/orders_data.csv"
+
+    # Подкючение к базе данных north
+    conn = psycopg2.connect(host='localhost',
 					database="north",
                     user='postgres',
 					password='sql2023',
                     port='5432'
-)
+    )
 
-conn.autocommit = True
-cursor = conn.cursor()
+    conn.autocommit = True
+    cursor = conn.cursor()
 
-# Копирование данных из файла employees_data.csv
-sql = '''COPY employees(employee_id,first_name,\
-last_name,title,birth_date,notes)
-FROM employees_csv
-DELIMITER ','
-CSV HEADER;'''
+    # Копирование данных из файла employees_data.csv
+    sql = '''COPY employees(employee_id,first_name,\
+    last_name,title,birth_date,notes)
+    FROM employees_csv
+    DELIMITER ','
+    CSV HEADER;'''
 
-cursor.execute(sql)
+    cursor.execute(sql)
 
-sql_1 = '''select * from employees;'''
-cursor.execute(sql_1)
-for i in cursor.fetchall():
-    print(i)
+    sql_1 = '''select * from employees;'''
+    cursor.execute(sql_1)
+    for i in cursor.fetchall():
+        print(i)
 
-# Копирование данных из файла customers_data.csv
-sql_2 = '''COPY customers(customer_id,company_name,\
-contact_name)
-FROM employees_csv
-DELIMITER ','
-CSV HEADER;'''
+    # Копирование данных из файла customers_data.csv
+    sql_2 = '''COPY customers(customer_id,company_name,\
+    contact_name)
+    FROM employees_csv
+    DELIMITER ','
+    CSV HEADER;'''
 
-cursor.execute(sql_2)
+    cursor.execute(sql_2)
 
-sql_3 = '''select * from customers;'''
-cursor.execute(sql_3)
-for i in cursor.fetchall():
-    print(i)
+    sql_3 = '''select * from customers;'''
+    cursor.execute(sql_3)
+    for i in cursor.fetchall():
+        print(i)
 
-# Копирование данных из файла orders_data.csv
-sql_4 = '''COPY orders(order_id,customer_id,\
-employee_id,order_date,ship_city)
-FROM orders_csv
-DELIMITER ','
-CSV HEADER;'''
+    # Копирование данных из файла orders_data.csv
+    sql_4 = '''COPY orders(order_id,customer_id,\
+    employee_id,order_date,ship_city)
+    FROM orders_csv
+    DELIMITER ','
+    CSV HEADER;'''
 
-cursor.execute(sql_4)
+    cursor.execute(sql_4)
 
-sql_5 = '''select * from orders;'''
-cursor.execute(sql_5)
-for i in cursor.fetchall():
-    print(i)
+    sql_5 = '''select * from orders;'''
+    cursor.execute(sql_5)
+    for i in cursor.fetchall():
+        print(i)
 
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
