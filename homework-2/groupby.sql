1e81417f2e19ac765b0651ed9bf23e49eeea6e75
Vyacheslav Tr9khimyuk <tva2023github@gmail.com>
2023-08-09 03:35:51 +0300

    Add groupby.sql

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -1,12 +1,25 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. заказы, отправленные в города, заканчивающиеся на 'burg'. Вывести без повторений две колонки (город, страна) (см. таблица orders, колонки ship_city, ship_country)
-
+SELECT DISTINCT ship_city, ship_country
+FROM orders
+WHERE ship_city LIKE '%burg';
 
 -- 2. из таблицы orders идентификатор заказа, идентификатор заказчика, вес и страну отгрузки. Заказ отгружен в страны, начинающиеся на 'P'. Результат отсортирован по весу (по убыванию). Вывести первые 10 записей.
-
+SELECT order_id, customer_id, freight, ship_country
+FROM orders
+WHERE ship_country LIKE 'P%'
+ORDER BY freight DESC
+LIMIT 10;
 
 -- 3. фамилию, имя и телефон сотрудников, у которых в данных отсутствует регион (см таблицу employees)
+SELECT last_name, first_name, home_phone
+FROM employees
+WHERE region IS NULL;
 
+# согласно скриншоту:
+SELECT first_name, last_name, home_phone
+FROM employees
+WHERE region IS NULL;
 
 -- 4. количество поставщиков (suppliers) в каждой из стран. Результат отсортировать по убыванию количества поставщиков в стране
 
