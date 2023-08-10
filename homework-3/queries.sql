883308a4553b64b9c5c405019160006a6b7791f9
Grandic <d.bitkov@gmail.com>
2023-08-10 22:17:00 +0300

    HT3 was over

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,29 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
+SELECT customers.company_name as customer, CONCAT(employees.first_name, ' ', employees.last_name) as employee
+FROM orders
+INNER JOIN customers USING(customer_id)
+INNER JOIN employees USING(employee_id)
+WHERE employees.city='London' and employees.city=customers.city and ship_via=2
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone from products
+INNER JOIN suppliers USING(supplier_id)
+INNER JOIN categories USING(category_id)
+WHERE products.discontinued=0 and categories.category_name in ('Dairy Products', 'Condiments') and units_in_stock <25
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name from customers
+WHERE NOT EXISTS(SELECT order_id FROM orders WHERE orders.customer_id=customers.customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+select DISTINCT product_name from products
+INNER JOIN order_details USING(product_id)
+WHERE quantity IN (SELECT quantity from order_details) and quantity=10
