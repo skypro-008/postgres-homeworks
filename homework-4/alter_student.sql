f555c761125588c5308b9ffa9dbb965550821bfb
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2023-08-13 22:03:17 +0300

    first commit HW-4

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -19,7 +19,7 @@ ALTER TABLE student ADD COLUMN middle_name varchar;
 ALTER TABLE student DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-ALTER TABLE student rename birthday TO birth_date;
+ALTER TABLE student RENAME birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
 
