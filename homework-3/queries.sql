e1fdc3f0112d9e5bbe0981724e7f798eaf2bd01c
igorzinoviev08 <zinovievigor08@gmail.com>
2023-08-28 16:36:56 +0300

    Все запросы выполнены

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,38 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT c.company_name, CONCAT(e.first_name, ' ', e.last_name)
+FROM orders AS o
+JOIN customers AS c USING(customer_id)
+JOIN employees AS e USING(employee_id)
+JOIN shippers AS s ON s.shipper_id = o.ship_via
+WHERE c.city = 'London' and e.city = 'London' and s.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products AS p
+JOIN suppliers AS s USING(supplier_id)
+JOIN categories AS c USING(category_id)
+WHERE c.category_name IN ('Dairy Products', 'Condiments') AND p.discontinued = 0 AND p.units_in_stock < 25
+ORDER BY p.units_in_stock;
 
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT c.company_name
+FROM customers AS c
+LEFT JOIN orders USING(customer_id)
+WHERE order_id IS NULL
+ORDER BY company_name;
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT product_name
+FROM products
+WHERE product_id = ANY (
+						SELECT product_id
+						FROM order_details
+						WHERE quantity = 10
+						);
