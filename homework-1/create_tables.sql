b3c8e3337fe27c414483f6155c639111a756a16e
Yulya Govorova <tishina1512@mail.ru>
2023-09-10 00:22:05 +1100

    added tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+    employee_id int PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+	birth_date DATE NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers (
+    customer_id VARCHAR PRIMARY KEY,
+    company_name VARCHAR(100) NOT NULL,
+    contact_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id VARCHAR REFERENCES customers(customer_id),
+    employee_id INT REFERENCES employees(employee_id),
+    order_date DATE NOT NULL,
+    ship_city VARCHAR(50) NOT NULL
+);
