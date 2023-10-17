12e51ef57b39ac968e38f19dcd284df0755ef506
JL34 <johnny2791@seznam.cz>
2023-10-17 13:26:21 +0200

    homework 1 done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+    first_name varchar(100),
+    last_name varchar(100),
+    title varchar(100),
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(5),
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5),
+	employee_id int,
+	order_date date,
+	ship_city varchar(100)
+);
