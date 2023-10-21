be64030cef6c30d22b24e4b97f1b491297cb65d9
Sergei <you@example.com>
2023-10-21 18:49:50 +0800

    homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title text NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5) UNIQUE PRIMARY KEY,
+	company_name text NOT NULL,
+	contact_name text NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+    employee_id INT REFERENCES employees(employee_id) NOT NULL,
+    order_date DATE NOT NULL,
+    ship_city TEXT
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,57 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+pw_db: str = os.getenv("PW_DB")
+
+customers_path = 'north_data/customers_data.csv'
+employees_path = 'north_data/employees_data.csv'
+orders_path = 'north_data/orders_data.csv'
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password=pw_db)
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open(customers_path) as customers:
+                customers_read = csv.DictReader(customers)
+                for row in customers_read:
+                    # execute querty
+                    cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                (row['customer_id'],
+                                 row['company_name'],
+                                 row['contact_name']
+                                ))
+
+            with open(employees_path) as employees:
+                employees_read = csv.DictReader(employees)
+                for row in employees_read:
+                    # execute querty
+                    cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                (row['employee_id'],
+                                 row['first_name'],
+                                 row['last_name'],
+                                 row['title'],
+                                 row['birth_date'],
+                                 row['notes']
+                                 ))
+
+            with open(orders_path) as orders:
+                orders_read = csv.DictReader(orders)
+                for row in orders_read:
+                    # execute querty
+                    cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                                (row['order_id'],
+                                 row['customer_id'],
+                                 row['employee_id'],
+                                 row['order_date'],
+                                 row['ship_city']
+                                ))
+
+finally:
+    conn.close()
