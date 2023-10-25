1f6efe3fd0e38fdb32e640fbfd1463f90380aebc
Boris <borik1and@gmail.com>
2023-10-25 19:00:07 +0300

    add def open_csv_file

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,12 @@
+import psycopg2
+import csv
+
+def open_csv_file(file):
+    with open(file, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        return csv_reader
+
+
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+
