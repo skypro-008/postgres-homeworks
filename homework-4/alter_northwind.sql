eb193b96155302f251d44816bf8bbfd163ab7f75
shwwdnl <shwwdnl@gmail.com>
2023-11-03 15:30:14 +0500

    update file

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -14,7 +14,7 @@ FROM public.products
 WHERE discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
+DELETE FROM public.products WHERE discontinued = 1;
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
 
 DELETE FROM public.order_details WHERE product_id IN (SELECT product_id FROM public.products WHERE discontinued = 1);
-DELETE FROM public.products WHERE discontinued = 1;
