1e030c5570d447860982610ee8b7bebfde6d1061
denisbelenko <denisdel123@>
2023-11-13 19:03:48 +0800

    удалил код для обращения к базе данных

--- homework-1/main.py
+++ homework-1/main.py
@@ -13,12 +13,7 @@ if __name__ == '__main__':
     try:
         with conn:
             with conn.cursor() as cur:
-                cur.execute('CREATE TABLE customers (customer_id varchar(30) PRIMARY KEY, company_name \
-                varchar(100) NOT NULL, contact_name varchar(100));')
-
-                cur.execute('CREATE TABLE employees(employees_id int PRIMARY KEY, first_name varchar(50) NOT NULL, \
-                last_name varchar(50) NOT NULL, title varchar(100) NOT NULL, birth_date varchar(50), \
-                notes text NOT NULL)')
+                cur.execute()
 
     finally:
         conn.close()
