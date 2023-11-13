8863aef0270c7bd8074cb87d93f743b5eff72bee
denisbelenko <denisdel123@>
2023-11-13 18:47:43 +0800

    создал таблицы в pgAdmin добавил модуль psycopg2 соеденился с базой данных

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE DATABASE north;
+
+CREATE TABLE customers (customer_id varchar(30) PRIMARY KEY,
+						company_name varchar(100) NOT NULL,
+						contact_name varchar(100) NOT NULL
+					   )
+
+CREATE TABLE employees(employees_id int PRIMARY KEY,
+					   first_name varchar(50) NOT NULL,
+					   last_name varchar(50) NOT NULL,
+					   title varchar(100) NOT NULL,
+					   birth_date varchar(50),
+					   notes text NOT NULL
+					  );
+
+CREATE TABLE orders (
+	order_id int PRIMARY KEY,
+	customer_id varchar(30) UNIQUE REFERENCES customers(customer_id),
+	employees_id int UNIQUE REFERENCES employees(employees_id),
+	order_date varchar(50) NOT NULL,
+	ship_city varchar(100)
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+if __name__ == '__main__':
+
+    conn = psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="55555"
+    )
+
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute('CREATE TABLE customers (customer_id varchar(30) PRIMARY KEY, company_name \
+                varchar(100) NOT NULL, contact_name varchar(100));')
+
+                cur.execute('CREATE TABLE employees(employees_id int PRIMARY KEY, first_name varchar(50) NOT NULL, \
+                last_name varchar(50) NOT NULL, title varchar(100) NOT NULL, birth_date varchar(50), \
+                notes text NOT NULL)')
+
+    finally:
+        conn.close()
+
+
+
