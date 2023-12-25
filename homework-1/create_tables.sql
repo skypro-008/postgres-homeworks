e50dddc8758f5cd00d5fab2d1a3fa5187dc80d72
Eugene Geym <geymzheka@gmail.com>
2023-12-25 15:46:38 +0500

    fixed

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,7 +1,7 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE customers
 (
-	customer_id char(5) NOT NULL,
+	customer_id char(5) PRIMARY KEY,
 	company_name varchar(50) NOT NULL,
 	contact_name varchar(50) NOT NULL,
 	UNIQUE (customer_id)
@@ -20,7 +20,7 @@ CREATE TABLE employees
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-	customer_id char(5) UNIQUE REFERENCES customers(customer_id) NOT NULL,
+	customer_id char(5) REFERENCES customers(customer_id),
 	employee_id int REFERENCES employees(employee_id) NOT NULL,
 	order_data date NOT NULL,
 	ship_city varchar(50) NOT NULL
