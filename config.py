81c8c104e52672c4b7ccd74b89f0b74d635b8401
Egor Nikitin <eganik5674@gmail.com>
2024-01-09 21:17:21 +0300

    Initial config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CUSTOMERS_PATH = Path(__file__).parent.joinpath("homework-1", "north_data", "customers_data.csv")
+EMPLOYEES_PATH = Path(__file__).parent.joinpath("homework-1", "north_data", "employees_data.csv")
+ORDERS_PATH = Path(__file__).parent.joinpath("homework-1", "north_data", "orders_data.csv")
