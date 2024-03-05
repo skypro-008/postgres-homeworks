78454c920fec4bc3fa8922109addf804407f40a1
Yurpa <rossiuslev@gmail.com>
2024-03-05 20:15:16 +0200

    From pgAdmin

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+Create TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+Create TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+Create TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(50),
+	UNIQUE(customer_id)
+)
