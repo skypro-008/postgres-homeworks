82830c2302340874944965bc641360761e139e6a
Yurpa <rossiuslev@gmail.com>
2024-03-05 20:46:44 +0200

    Added all the python code

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,21 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+
+create_tables = 'create_tables.sql'
+employees = os.path.abspath('north_data/employees_data.csv')
+orders = os.path.abspath('north_data/orders_data.csv')
+customers = os.path.abspath('north_data/customers_data.csv')
+
+
+#connection
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='19182521'
+)
+
+def main():
+    conn.close()
