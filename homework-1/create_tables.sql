4d8cf9b5f25f738350b10cf88f4ce900c2c4b58e
DarkRazec <darkrazec@yahoo.com>
2024-03-20 20:32:23 +0300

    homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE IF NOT EXISTS customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name text NOT NULL,
+	contact_name text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees
+(
+	employee_id smallserial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL DEFAULT CURRENT_DATE,
+	notes text
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+	order_id serial PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id),
+	employee_id smallserial REFERENCES employees(employee_id),
+	order_date date NOT NULL DEFAULT CURRENT_DATE,
+	ship_city varchar(50) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,31 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn_params = {
+    "host": "localhost",
+    "database": "north",
+    "user": "postgres",
+    "password": "89054313192"
+}
+
+with open("north_data/customers_data.csv", newline='', encoding='UTF-8') as csv_f:
+    dict_data = csv.reader(csv_f)
+    customers_data = [row for row in dict_data]
+    customers_data.pop(0)
+with open("north_data/employees_data.csv", newline='', encoding='UTF-8') as csv_f:
+    dict_data = csv.reader(csv_f)
+    employees_data = [row for row in dict_data]
+    employees_data.pop(0)
+with open("north_data/orders_data.csv", newline='', encoding='UTF-8') as csv_f:
+    dict_data = csv.reader(csv_f)
+    orders_data = [row for row in dict_data]
+    orders_data.pop(0)
+with psycopg2.connect(**conn_params) as conn:
+    with conn.cursor() as cur:
+        insert = "INSERT INTO customers VALUES (%s, %s, %s);"
+        [cur.execute(insert, (customer[0], customer[1], customer[2])) for customer in customers_data]
+        insert = "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s);"
+        [cur.execute(insert, (empl[0], empl[1], empl[2], empl[3], empl[4], empl[5])) for empl in employees_data]
+        insert = "INSERT INTO orders VALUES (%s, %s, %s, %s, %s);"
+        [cur.execute(insert, (order[0], order[1], order[2], order[3], order[4])) for order in orders_data]
