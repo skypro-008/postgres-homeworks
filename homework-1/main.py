4bfaba1c0f13a398bf834dc64cb4fa987ba83b5a
DuckRainbow <sillestyna@gmail.com>
2024-03-25 22:17:58 +0500

    func is added

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,19 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import csv
+import os
 
 import psycopg2
 from config import EMPLOYEES_DATA_PATH, CUSTOMERS_DATA_PATH, ORDERS_DATA_PATH
+
+
+def func_fill(cur, file, table, values):
+    with open(file, 'r', encoding='UTF-8') as f:
+        data = csv.DictReader(f)
+        for row in data:
+            cur.execute(
+                f'INSERT INTO {table} VALUES({values})',
+                (row.values())
+            )
 
 
 def fill_tables():
@@ -11,36 +22,15 @@ def fill_tables():
         host='localhost',
         database='north',
         user='postgres',
-        password='Iwilldietomorrow92815'
+        password=os.environ['POSTGRESKEY']
+
     )
     try:
         with conn:
             with conn.cursor() as cur:
-                with open('north_data/customers_data.csv', 'r', encoding='UTF-8') as f:
-                    data = csv.DictReader(f)
-                    for row in data:
-                        cur.execute(
-                            'INSERT INTO customers VALUES(%s, %s, %s)',
-                            (row['customer_id'], row['company_name'], row['contact_name'])
-                        )
-
-                with open('north_data/employees_data.csv', 'r', encoding='UTF-8') as f:
-                    data = csv.DictReader(f)
-                    for row in data:
-                        cur.execute(
-                            'INSERT INTO employees VALUES(%s, %s, %s, %s, %s, %s)',
-                            (row['employee_id'], row['first_name'], row['last_name'],
-                             row['title'], row['birth_date'], row['notes'])
-                        )
-
-                with open('north_data/orders_data.csv', 'r', encoding='UTF-8') as f:
-                    data = csv.DictReader(f)
-                    for row in data:
-                        cur.execute(
-                            'INSERT INTO orders VALUES(%s, %s, %s, %s, %s)',
-                            (row['order_id'], row['customer_id'], row['employee_id'],
-                             row['order_date'], row['ship_city'])
-                        )
+                func_fill(cur, 'north_data/customers_data.csv', 'customers', '%s, %s, %s')
+                func_fill(cur, 'north_data/employees_data.csv', 'employees', '%s, %s, %s, %s, %s, %s')
+                func_fill(cur, 'north_data/orders_data.csv', 'orders', '%s, %s, %s, %s, %s')
 
     finally:
         conn.close()
