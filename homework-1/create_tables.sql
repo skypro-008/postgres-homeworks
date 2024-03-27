51ee64f416e933ba286a743a43560d027f29b36b
Rf <jonnyzver78@gmail.com>
2024-03-28 00:14:24 +1000

    add files

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,21 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+    employee_id INT PRIMARY KEY,
+	first_name VARCHAR(15) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	title VARCHAR(50) NOT NULL,
+	birth_date DATE NOT NULL,
+	notes TEXT
+);
+CREATE TABLE customers(
+    customer_id CHAR(5) PRIMARY KEY,
+	company_name VARCHAR(50) NOT NULL,
+	contact_name VARCHAR(50) NOT NULL
+);
+CREATE TABLE orders(
+    order_id INT PRIMARY KEY,
+	customer_id CHAR(5) REFERENCES customers(customer_id),
+	employee_id INT REFERENCES employees(employee_id),
+	order_date DATE NOT NULL,
+	ship_city VARCHAR(50) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,45 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import os
+
+import psycopg2
+
+ROOT_DIR = os.path.dirname(__file__)
+DATA_PATH = os.path.join(ROOT_DIR, 'north_data')
+EMPLOYEES_DATA_PATH = os.path.join(ROOT_DIR, 'north_data', 'employees_data.csv')
+CUSTOMERS_DATA_PATH = os.path.join(ROOT_DIR, 'north_data', 'customers_data.csv')
+ORDERS_DATA_PATH = os.path.join(ROOT_DIR, 'north_data', 'orders_data.csv')
+
+
+def func_fill(cur, file, table, value):
+    with open(file, 'r', encoding='UTF-8') as f:
+        data = csv.DictReader(f)
+        for row in data:
+            cur.execute(
+                f'INSERT INTO {table} VALUES({value})',
+                (list(row.values()))
+            )
+
+
+def fill_tables():
+    # connect to db
+    conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password=os.environ["220913"]
+
+    )
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                func_fill(cur, 'north_data/customers_data.csv', 'customers', '%s, %s, %s')
+                func_fill(cur, 'north_data/employees_data.csv', 'employees', '%s, %s, %s, %s, %s, %s')
+                func_fill(cur, 'north_data/orders_data.csv', 'orders', '%s, %s, %s, %s, %s')
+
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    fill_tables()
